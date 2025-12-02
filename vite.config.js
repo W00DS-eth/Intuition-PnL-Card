4d7d14ba1d54cb6ccd14b865e17b67b359d21e94
Jeremie <socials.jeremie.olivier@gmail.com>
2025-12-03 02:11:36 +0800

    change base URL for the new domain : pnl.intuiton.box

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Intuition-PnL-Card/',
+  base: '/',
   build: {
     rollupOptions: {
       onwarn(warning, warn) {
